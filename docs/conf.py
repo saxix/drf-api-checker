e005e0cf90d5aed65472eef5de7fe1d9a753150a
sax <s.apostolico@gmail.com>
2018-06-14 07:17:56 +0200

    removes django dependency from docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,9 @@ import sys, os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
-from django.conf import settings
-
-settings.configure()
+# from django.conf import settings
+#
+# settings.configure()
 
 import drf_api_checker
 

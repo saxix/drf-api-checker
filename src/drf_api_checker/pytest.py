f5e382748f48ccfd1097c2ce2bca68c99cf43b1a
saxix <s.apostolico@gmail.com>
2019-08-16 14:31:07 +0200

    fixes pytest support

--- src/drf_api_checker/pytest.py
+++ src/drf_api_checker/pytest.py
@@ -16,14 +16,12 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(autouse=True, scope="session")
-def configure_env(request, pytestconfig):
+def configure_env(request):
     option = False
-    if pytestconfig.getoption("reset_contracts"):
-        option = 1
-    elif hasattr(pytest, 'config'):
+    if hasattr(pytest, 'config'):
         option = pytest.config.option.reset_contracts
-    elif hasattr(request, 'option'):
-        option = request.option.reset_contracts
+    elif hasattr(request, 'config'):
+        option = request.config.option.reset_contracts
 
     if option:
         os.environ['API_CHECKER_RESET'] = "1"
